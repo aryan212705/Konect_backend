c0277748ec7a3f4c4af801e88700ca9cc3d639af
Aryan <aryangarg212705@gmail.com>
2019-12-04 23:13:26 +0530

    Database setting changes

--- groups/models.py
+++ groups/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 
 class Groups(models.Model):
+    id = models.BigIntegerField(primary_key = True)
     groupName = models.CharField('Group Name', max_length=50)
     creationDate = models.DateField('Group Creation Date', default=datetime.now)
 
