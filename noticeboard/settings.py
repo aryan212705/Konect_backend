2a5f9f1f4029d0081147d125346f52afb77451b2
Aryan <aryangarg212705@gmail.com>
2019-12-04 22:41:44 +0530

    Reset Database

--- noticeboard/settings.py
+++ noticeboard/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'noticeboard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db1.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
